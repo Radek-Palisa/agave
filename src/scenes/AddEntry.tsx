d5ec4e4a4a38f0852129f4617f2e08562ec45f11
Radek Palisa <radek.palisa@gmail.com>
2021-09-15 18:50:56 +0300

    cleanup

--- src/scenes/AddEntry.tsx
+++ src/scenes/AddEntry.tsx
@@ -1,9 +1,8 @@
 import React, { useRef } from 'react';
 import { RouteComponentProps } from '@reach/router';
-// import store from '../store';
 import Editor from './Editor/Editor';
 import { Entry, PostEntryPayload } from '../types';
-// import { useError } from '../providers/ErrorProvider';
+import { useError } from '../providers/ErrorProvider';
 import { ROUTES } from '../consts';
 import { useCurrentEntry } from '../providers/CurrentEntryProvider';
 import store from '../store';
@@ -18,36 +17,7 @@ const initialValues: Entry = {
 export default function AddEntry(_: RouteComponentProps) {
   const isCreated = useRef(false);
   const { data, isLoading, saveEntryRemotely, setCurrentEntry } = useCurrentEntry();
-  // const [, setError] = useError();
-
-  // useEffect(() => {
-  //   return () => {
-  //     if (!navigate) return;
-
-  //     saveEntryRemotely(async ({ id: _, ...values }: Entry) => {
-  //       // TODO - this meh, re-work it
-  //       // reset current entry on the component unmount
-  //       setCurrentEntry(null);
-
-  //       // when left blank, skip creating the entry
-  //       if (!values.title && !values.text && values.tags.length === 0) {
-  //         return;
-  //       }
-
-  //       store.addEntry(values);
-  //     }).catch(error => {
-  //       setError({
-  //         message: `There's been a problem while saving the entry.`,
-  //         additionalInfo: error.message,
-  //       });
-
-  //       // By the time this error manifests, the user has already been
-  //       // redirected away from the add entry page, so navigate them
-  //       // back so that they can re-try
-  //       navigate(ROUTES.ADD_ENTRY);
-  //     });
-  //   };
-  // }, []);
+  const [, setError] = useError();
 
   const handleChange = (values: PostEntryPayload) => {
     if (!values.title && !values.text && values.tags.length === 0) {
@@ -57,12 +27,20 @@ export default function AddEntry(_: RouteComponentProps) {
     if (!isCreated.current) {
       isCreated.current = true;
 
-      return store.addEntry(values).then(res => {
-        setCurrentEntry({
-          id: res.id,
-          ...values,
+      return store
+        .addEntry(values)
+        .then(res => {
+          setCurrentEntry({
+            id: res.id,
+            ...values,
+          });
+        })
+        .catch(error => {
+          setError({
+            message: `There's been a problem while saving the entry.`,
+            additionalInfo: error.message,
+          });
         });
-      });
     }
 
     return saveEntryRemotely(values);
--- src/scenes/EditEntry.tsx
+++ src/scenes/EditEntry.tsx
@@ -1,34 +1,22 @@
-import { RouteComponentProps } from '@reach/router';
 import React from 'react';
+import { RouteComponentProps } from '@reach/router';
 import Editor from './Editor/Editor';
-// import store from '../store';
-// import { useError } from '../providers/ErrorProvider';
-// import { ROUTES } from '../consts';
+import { useError } from '../providers/ErrorProvider';
 import { useCurrentEntry } from '../providers/CurrentEntryProvider';
+import { PostEntryPayload } from '../types';
 
 export default function EditEntry(_: RouteComponentProps) {
-  // const [, setError] = useError();
+  const [, setError] = useError();
   const { data, isLoading, saveEntryRemotely } = useCurrentEntry();
 
-  // useEffect(() => {
-  //   return () => {
-  //     if (!navigate) return;
-
-  //     saveEntryRemotely(store.editEntry).catch(error => {
-  //       setError({
-  //         message: `There's been a problem while saving the entry.`,
-  //         additionalInfo: error.message,
-  //       });
-
-  //       // By the time this error manifests, the user has already been
-  //       // redirected away from the edit entry page, so navigate them
-  //       // back so that they can re-try
-  //       if (error._entryId) {
-  //         navigate(ROUTES.getEntryEditPath(error._entryId));
-  //       }
-  //     });
-  //   };
-  // }, []);
+  const handleChange = (values: PostEntryPayload) => {
+    return saveEntryRemotely(values).catch(error => {
+      setError({
+        message: `There's been a problem while saving the entry.`,
+        additionalInfo: error.message,
+      });
+    });
+  };
 
   if (isLoading) {
     return <p>Loading...</p>;
@@ -43,7 +31,7 @@ export default function EditEntry(_: RouteComponentProps) {
       backLinkProps={{ to: `/entry/${data.id}` }}
       navTitle="Edit Entry"
       initialValues={data}
-      onChange={saveEntryRemotely}
+      onChange={handleChange}
     />
   );
 }
